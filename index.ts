9a048ac4b9c716c0ce5714dc8cc7e3559f65c8ea
Joseph Dorman <JDorman555@yahoo.com>
2025-02-05 16:38:53 -0600

    replace require with imports

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
-require("dotenv").config();
-const express = require("express");
+import "dotenv/config";
+import express from "express";
+import usersRouter from "./routes/usersRouter";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
