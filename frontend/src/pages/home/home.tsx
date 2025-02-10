03dbdd4b83c00b4daf7c3c7518998659965bacde
Joseph Dorman <JDorman555@yahoo.com>
2025-02-10 15:06:37 -0600

    fix spacing and routes

--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 function Home() {
   return (
     <>
@@ -12,13 +14,17 @@ function Home() {
             Upload, organize and share files using a persistant 24/7 hosting
             service. Easy to use, secure fast downloads and uploads.
           </p>
-          <div className="grid gap-4">
-            <button className="bg-purple py-4 px-16 rounded-sm text-xs font-bold transition duration-150 ease-in-out hover:bg-purple-hover hover:cursor-pointer">
-              GET STARTED
-            </button>
-            <button className="text-grey-text text-xs transition duration-150 ease-in-out hover:text-purple-accent hover:cursor-pointer">
-              Sign In
-            </button>
+          <div className="flex flex-col gap-4">
+            <Link to="/signup">
+              <button className="bg-purple py-4 px-16 rounded-sm text-xs font-bold transition duration-150 ease-in-out hover:bg-purple-hover hover:cursor-pointer">
+                GET STARTED
+              </button>
+            </Link>
+            <Link className="w-12 self-center" to="/login">
+              <button className="text-grey-text text-xs transition duration-150 ease-in-out hover:text-purple-accent hover:cursor-pointer">
+                Sign In
+              </button>
+            </Link>
           </div>
         </div>
       </main>
