782cb99cb4362a60b78772f692aeb02efede90a7
Joseph Dorman <JDorman555@yahoo.com>
2025-02-14 15:40:23 -0600

    change api endpoint

--- backend/index.ts
+++ backend/index.ts
@@ -9,8 +9,15 @@ const PORT = process.env.PORT || 3000;
 app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
 app.use(express.urlencoded({ extended: false }));
 
-// change endpoint to start with /api/v1
-app.use("/", usersRouter);
-app.use("/files", filesRouter);
+// Can route to a single route of /api/v1 to improve scalability and redundancy
+// Of declaring the default endpoint on every route.
+
+app.get("/", (req, res) => res.redirect("/api/v1"));
+app.get("/api/v1", (req, res) => {
+  console.log("index");
+  res.send("Api endpoint for Warp Files.");
+});
+app.use("/api/v1/users", usersRouter);
+app.use("/api/v1/files", filesRouter);
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
--- backend/routes/usersRouter.ts
+++ backend/routes/usersRouter.ts
@@ -2,10 +2,6 @@ import { Router } from "express";
 import * as usersController from "../controllers/usersController";
 const usersRouter = Router();
 
-usersRouter.get("/", (req, res) => {
-  res.send("This is the home index page of file upload.");
-});
-
 usersRouter.get("/logout", usersController.logOut);
 usersRouter.post("/login", usersController.logIn);
 usersRouter.post("/signup", usersController.signUp);
