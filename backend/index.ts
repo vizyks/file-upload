e0affd4bb78b1044a817843ed48bbcd22385747c
Joseph Dorman <JDorman555@yahoo.com>
2025-02-14 14:51:01 -0600

    install and configure cors

--- backend/index.ts
+++ backend/index.ts
@@ -1,12 +1,15 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
 import usersRouter from "./routes/usersRouter";
 import filesRouter from "./routes/filesRouter";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
 app.use(express.urlencoded({ extended: false }));
 
+// change endpoint to start with /api/v1
 app.use("/", usersRouter);
 app.use("/files", filesRouter);
 
